22843069c12306da411630d1e1b711ff03638a1d
99blanc <t990421@naver.com>
2025-11-27 22:41:54 +0900

    [Feat] 예제 2-1 학습

---
+++ ch2n1/Hello.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace ch2n1
+{
+    public class Hello
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("사용법: Hello.exe <이름>");
+                return;
+            }
+
+            WriteLine("Hello, {0}!", args[0]);
+        }
+    }
+}
