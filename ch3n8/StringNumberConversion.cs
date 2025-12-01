2190c15bad38f6a13d576524f32bde4d6cd7b464
99blanc <t990421@naver.com>
2025-12-01 20:59:31 +0900

    [Feat] 예제 3-8 학습

---
+++ ch3n8/StringNumberConversion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ch3n8
+{
+    public class StringNumberConversion
+    {
+        public static void Main(string[] args)
+        {
+            int a = 123;
+            string b = a.ToString();
+            Console.WriteLine(b);
+
+            float c = 3.14f;
+            string d = c.ToString();
+            Console.WriteLine(d);
+
+            string e = "1.2345";
+            float h = float.Parse(g);
+            Console.WriteLine(h);
+        }
+    }
+}
