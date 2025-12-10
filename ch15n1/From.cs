3b45075fcc2f1fb3e4ae56a358203d23ba630dad
99blanc <t990421@naver.com>
2025-12-10 19:12:08 +0900

    [Feat] 예제 15-1 학습

---
+++ ch15n1/From.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace ch15n1
+{
+    public class From
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = { 9, 2, 6, 4, 5, 3, 7, 8, 1, 10 };
+
+            var result = from n in numbers
+                         where n % 2 == 0
+                         orderby n
+                         select n;
+
+            foreach (int n in numbers)
+            {
+                Console.WriteLine($"짝수 : {n}");
+            }
+        }
+    }
+}
