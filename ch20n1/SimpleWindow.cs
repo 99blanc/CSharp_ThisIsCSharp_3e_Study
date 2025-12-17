784798c1384222ea132ad6b25b5a9a251bac14ca
99blanc <t990421@naver.com>
2025-12-17 22:17:26 +0900

    [Feat] 예제 20-1 학습

---
+++ ch20n1/SimpleWindow.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ch20n1;
+
+public class SimpleWindow : System.Windows.Forms.Form
+{
+    public static void Main(string[] args)
+    {
+        System.Windows.Forms.Application.Run(new SimpleWindow());
+    }
+}
